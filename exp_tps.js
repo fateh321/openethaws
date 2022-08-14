7c71bf86cb794bcbe3b3614fd934ec00ed7e33ee
srisht <fateh321@gmail.com>
2022-08-14 01:21:43 -0400

    minor

--- exp_tps.js
+++ exp_tps.js
@@ -3,8 +3,8 @@ const csv = require('csv-parser');
 const Web3 = require('web3');
 const fs = require('fs');
 const path = require("path");
-// const web3 = new Web3('http://localhost:8540');
-const web3 = new Web3('172.31.39.234:8541');
+const web3 = new Web3('http://localhost:8540');
+// const web3 = new Web3('172.31.39.234:8541');
 
 var queryParameter = ()=> new Promise( resolve =>{
 	var keys = [];
@@ -24,7 +24,43 @@ queryParameter().then((res)=>
 	console.log("fuck you");
 	let len = keys.length;
 	console.log(len);
-	for (let i = 0; i < 8000; i++) {
+	const t = 4000
+	for (let i = 0; i < t; i++) {
+		const addressTo = '0x65e154ef9a2967e922936415bb0e2204be87b64c';
+		// Initialization
+		const privKey = keys[i].Privkey; // Genesis private key
+		const addressFrom = keys[i].PubKey;
+
+		const deploy = async () => {
+		   console.log(
+		      `Attempting to make transaction from ${addressFrom} to ${addressTo}`
+		   );
+
+		   const createTransaction = await web3.eth.accounts.signTransaction(
+		      {
+		         from: addressFrom,
+		         to: addressTo,
+		         value: web3.utils.toWei('1', 'ether'),
+		         gas: '21000',
+		      },
+		      privKey
+		   );
+
+		   // Deploy transaction
+		   const createReceipt = await web3.eth.sendSignedTransaction(
+		      createTransaction.rawTransaction
+		   );
+		   // console.log(
+		   //    `Transaction successful with hash: ${createReceipt.transactionHash}`
+		   // );
+		};
+
+		deploy();
+
+
+
+	}
+		for (let i = t; i < 2*t; i++) {
 		const addressTo = '0x65e154ef9a2967e922936415bb0e2204be87b64c';
 		// Initialization
 		const privKey = keys[i].Privkey; // Genesis private key
