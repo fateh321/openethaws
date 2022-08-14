88c8e00c78cfd84d8f1a4eb4377ab291c745c6a0
srisht <fateh321@gmail.com>
2022-08-14 19:13:40 -0400

    remove print

--- crates/hyperproofs/src/lib.rs
+++ crates/hyperproofs/src/lib.rs
@@ -263,9 +263,9 @@ impl AggProof{
         4u64
     }
     pub fn txn_lifetime() -> u64 {
-        40u64
+        20u64
     }
-    pub fn incomplete_txn_buffer_length() -> u64{16000u64}
+    pub fn incomplete_txn_buffer_length() -> u64{32000u64}
     pub fn hyperproof_bits()-> u32{24u32}
     pub fn block_data_count() -> u64 {1024u64}
     // pub fn author_shard(address: Address) -> u64 {
