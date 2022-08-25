4939e2ee162508432f4e278ddd3ce4834159adce
srisht <fateh321@gmail.com>
2022-08-25 19:41:33 -0400

    mempool size doubled

--- crates/transaction-pool/src/options.rs
+++ crates/transaction-pool/src/options.rs
@@ -28,7 +28,7 @@ pub struct Options {
 impl Default for Options {
     fn default() -> Self {
         Options {
-            max_count: 1024,
+            max_count: 2048,
             max_per_sender: 16,
             max_mem_usage: 8 * 1024 * 1024,
         }
