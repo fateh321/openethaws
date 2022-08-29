7b5c909ee70726918edb99a84eda183cb7707ae5
srisht <fateh321@gmail.com>
2022-08-29 18:19:53 -0400

    2 shards

--- crates/hyperproofs/src/lib.rs
+++ crates/hyperproofs/src/lib.rs
@@ -225,17 +225,17 @@ impl AggProof{
             0u64},
             x if x==_s2 => unsafe{SHARD = 1u64;
                 1u64},
-            x if x==_s3 => unsafe{SHARD = 2u64;
+            x if x==_s3 => unsafe{SHARD = 0u64;
                 2u64},
-            x if x==_s4 => unsafe{SHARD = 3u64;
+            x if x==_s4 => unsafe{SHARD = 1u64;
                 3u64},
             x if x==_s5 => unsafe{SHARD = 0u64;
                 0u64},
             x if x==_s6 => unsafe{SHARD = 1u64;
                 1u64},
-            x if x==_s7 => unsafe{SHARD = 2u64;
+            x if x==_s7 => unsafe{SHARD = 0u64;
                 2u64},
-            x if x==_s8 => unsafe{SHARD = 3u64;
+            x if x==_s8 => unsafe{SHARD = 1u64;
                 3u64},
             _ => unsafe{SHARD = 999u64; println!("panic!, shard not recognised");
                 999u64},
@@ -321,7 +321,7 @@ impl AggProof{
     }
 
     pub fn shard_count() -> u64 {
-        4u64
+        2u64
     }
     pub fn node_count() -> u64 {
         8u64
