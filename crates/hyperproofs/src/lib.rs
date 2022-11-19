88e5d968ea43e84ef57c9053cd5d7122e7803013
srisht <fateh321@gmail.com>
2022-11-19 01:17:02 -0500

    16 nodes

--- crates/hyperproofs/src/lib.rs
+++ crates/hyperproofs/src/lib.rs
@@ -322,35 +322,35 @@ impl AggProof{
         match address {
             x if x==_s1  => unsafe{SHARD = 0u64;
             0u64},
-            x if x==_s2 => unsafe{SHARD = 0u64;
+            x if x==_s2 => unsafe{SHARD = 1u64;
                 1u64},
-            x if x==_s3 => unsafe{SHARD = 0u64;
+            x if x==_s3 => unsafe{SHARD = 2u64;
                 2u64},
-            x if x==_s4 => unsafe{SHARD = 0u64;
+            x if x==_s4 => unsafe{SHARD = 3u64;
                 3u64},
             x if x==_s5 => unsafe{SHARD = 0u64;
                 0u64},
-            x if x==_s6 => unsafe{SHARD = 0u64;
+            x if x==_s6 => unsafe{SHARD = 1u64;
                 1u64},
-            x if x==_s7 => unsafe{SHARD = 0u64;
+            x if x==_s7 => unsafe{SHARD = 2u64;
                 2u64},
-            x if x==_s8 => unsafe{SHARD = 0u64;
+            x if x==_s8 => unsafe{SHARD = 3u64;
                 3u64},
             x if x==_s9 => unsafe{SHARD = 0u64;
                 0u64},
-            x if x==_s10 => unsafe{SHARD = 0u64;
+            x if x==_s10 => unsafe{SHARD = 1u64;
                 1u64},
-            x if x==_s11 => unsafe{SHARD = 0u64;
+            x if x==_s11 => unsafe{SHARD = 2u64;
                 2u64},
-            x if x==_s12 => unsafe{SHARD = 0u64;
+            x if x==_s12 => unsafe{SHARD = 3u64;
                 3u64},
             x if x==_s13 => unsafe{SHARD = 0u64;
                 0u64},
-            x if x==_s14 => unsafe{SHARD = 0u64;
+            x if x==_s14 => unsafe{SHARD = 1u64;
                 1u64},
-            x if x==_s15 => unsafe{SHARD = 0u64;
+            x if x==_s15 => unsafe{SHARD = 2u64;
                 2u64},
-            x if x==_s16 => unsafe{SHARD = 0u64;
+            x if x==_s16 => unsafe{SHARD = 3u64;
                 3u64},
             _ => unsafe{SHARD = 999u64; println!("panic!, shard not recognised");
                 999u64},
@@ -476,10 +476,10 @@ impl AggProof{
     }
 
     pub fn shard_count() -> u64 {
-        1u64
+        4u64
     }
     pub fn node_count() -> u64 {
-        1u64
+        16u64
     }
     pub fn txn_lifetime() -> u64 {
         20u64
